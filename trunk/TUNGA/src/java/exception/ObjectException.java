681781e334f5da54c9e8eab59a2d4c1ce78b09c8
vongkhidau@gmail.com <vongkhidau@gmail.com@4046f316-fa27-2849-877a-0f805dfd5a6c>
2012-09-07 10:39:19 +0000

    qwe

---
+++ trunk/TUNGA/src/java/exception/ObjectException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ObjectException extends Exception {
+    public ObjectException(String message) {
+        super(message);
+    }
+}
